e7d7e221e36f03a5b48ca779f087ff070832a02c
DejanNikolic003 <knolekjhnny@gmail.com>
2025-04-09 09:09:14 +0200

    Added clearing the task-container after every task refresh, removed the whole code for creating dom elements, moved into domElements.js

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,4 +1,4 @@
-import { taskContainerEl, createButton, createBadge, createDomElement } from "./domElements";
+import { taskContainerEl, createTaskDomElements } from "./domElements";
 import Task from '../models/Task';
 
 const TASKS = [];
@@ -9,6 +9,7 @@ const initTasks = () => {
 };
 
 const refreshTasks = () => {
+    taskContainerEl.innerHTML = '';
     showTasks();
     handleEventListeners();
 };
@@ -20,41 +21,12 @@ const showTasks = () => {
     }
 
     taskContainerEl.append(...TASKS.map((task) => {
-        let taskWrapperEl = createDomElement('task', 'div', ['flex', 'items-center', 'justify-between', 'gap-2']);
-        let taskInfoWrapperEl = createDomElement('taskInfoWrapper', 'div');
-        let taskDateWrapper = createDomElement('taskDateWrapper', 'div');
-        let taskPriorityWrapper = createDomElement('taskPriorityWrapper', 'div');
-        let taskButtonsWrapper = createDomElement('taskButtonsWrapper', 'div', ['space-x-2']);
+        let taskElement = createTaskDomElements(task);
 
-        let taskNameEl = createDomElement('taskName', 'p');
-        let taskDescEl = createDomElement('taskDesc', 'p', ['text-sm', 'text-gray-500']);
-        let taskDateEl = createBadge('taskDate', 'p', ['bg-sky-100', 'text-sky-400']);
-        let taskPriorityEl = createBadge('taskPriority', 'p', ['bg-orange-100', 'text-orange-400']);
-        let taskEditButtonEl = createButton('editTaskButton', 'button', ['bg-emerald-500', 'hover:bg-emerald-600']);
-        let taskDoneButtonEl = createButton('doneTaskButton', 'button', ['bg-cyan-500', 'hover:bg-cyan-600']);
-        let taskDeleteButtonEl = createButton('deleteTaskButton', 'button', ['bg-red-500', 'hover:bg-red-600']);
-
-        taskWrapperEl.dataset.id = task.id;
-
-        taskNameEl.textContent = task.title;
-        taskDescEl.textContent = task.description;
-        taskDateEl.textContent = task.dueDate;
-        taskPriorityEl.textContent = task.priority;
-        taskEditButtonEl.textContent = 'Edit';
-        taskDoneButtonEl.textContent = 'Done';
-        taskDeleteButtonEl.textContent = 'Delete';
-
-
-        taskInfoWrapperEl.append(taskNameEl, taskDescEl);
-        taskDateWrapper.append(taskDateEl);
-        taskPriorityWrapper.append(taskPriorityEl);
-        taskButtonsWrapper.append(taskEditButtonEl, taskDeleteButtonEl, taskDoneButtonEl);
-
-        taskWrapperEl.append(taskInfoWrapperEl, taskDateWrapper, taskPriorityWrapper, taskButtonsWrapper);
-
-        return taskWrapperEl;
+        return taskElement;
     }));
 };
+
 
 const handleTaskDeletion = (event) => {
     let closestTask = event.target.closest('#task');
@@ -69,16 +41,34 @@ const handleTaskDeletion = (event) => {
     event.preventDefault();
 };
 
+const handleTaskStateChange = (event) => {
+    let closestTask = event.target.closest('#task');
+    let taskIndex = getTaskIndexById(closestTask.dataset.id);
+
+    if(taskIndex < 0) return;
+
+    TASKS[taskIndex].changeState()
+    
+    refreshTasks();
+
+    event.preventDefault();
+};
+
 const getTaskIndexById = (id) => {
     return TASKS.findIndex((task) => task.id === id);
 };
 
 const handleEventListeners = () => {
     let taskDeleteButtonsEl = document.querySelectorAll('#deleteTaskButton');
+    let taskDoneButtonEl = document.querySelectorAll('#doneTaskButton');
     let openModalButtonEl = document.querySelector('#openTaskModal');
 
     taskDeleteButtonsEl.forEach((button) => {
         button.addEventListener('click', handleTaskDeletion);
+    });
+
+    taskDoneButtonEl.forEach((button) => {
+        button.addEventListener('click', handleTaskStateChange);
     });
 
   
